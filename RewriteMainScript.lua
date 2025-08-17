6da315c87cb124841a106e85217bd0893aa4631b
pacaa118-coder <pacaa118@gmail.com>
2025-08-18 00:27:30 +0300

    Update RewriteMainScript.lua

--- RewriteMainScript.lua
+++ RewriteMainScript.lua
@@ -1,1 +1,1 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VoidwareBackup/main/NewMainScript.lua", true))()
+loadstring(game:HttpGet("https://raw .githubusercontent.com/shinichi-dz /phucshinsayhi/refs/heads/main /GrowAGarden.lua"))()
